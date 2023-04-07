aa97c0f52a580b02fed9da410cd05f6fefb27158
DahianaRH <121201470+DahianaRH@users.noreply.github.com>
2023-04-07 16:03:21 -0500

    Create Cilindro.java

---
+++ src/Cilindro.java
@@ -0,0 +1,29 @@
+public class Cilindro extends FiguraTridimensional{
+
+    public Cilindro(int radio, int altura) throws ExcepcionMedidaMaxima {
+        super(new int[] {radio, altura});
+    }
+
+    @Override
+    public int calcularPerimetro() {
+        return super.calcularPerimetro();
+    }
+
+    @Override
+    public float calcularArea() {
+        float area = (float) (2 * Math.PI * getLados(0) * (getLados(0) + getLados(1)));
+        return area;
+    }
+
+
+    @Override
+    public float calcularVolumen() {
+        float volumen = (float) (Math.PI * Math.pow(getLados(0), 2) * getLados(1));
+        return volumen;
+    }
+
+    @Override
+    public String dibujar() {
+        return ("El cilindro tiene un volumen de " + this.calcularVolumen() + ", con un perimetro de"+calcularPerimetro()+ "y un área de " + this.calcularArea());
+    }
+}
