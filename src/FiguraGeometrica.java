d6a905657688e7754a66254804331dcf8a2507df
DahianaRH <121201470+DahianaRH@users.noreply.github.com>
2023-04-07 13:57:20 -0500

    Update FiguraGeometrica.java

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -1,5 +1,19 @@
 public abstract class FiguraGeometrica {
     protected int[] lados;
+    
+    public FiguraGeometrica(int[] lados) throws ExcepcionMedidaMaxima {
+        int[] var2 = lados;
+        int var3 = lados.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            int lado = var2[var4];
+            if (lado > 10) {
+                throw new ExcepcionMedidaMaxima();
+            }
+        }
+
+        this.lados = lados;
+    }
 
     public abstract float calcularPerimetro();
     public abstract String dibujar();
