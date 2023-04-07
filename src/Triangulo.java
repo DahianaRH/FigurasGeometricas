9a7cc403a4fc39a41053dd105ad045611bbaee99
DahianaRH <121201470+DahianaRH@users.noreply.github.com>
2023-04-07 13:58:55 -0500

    Update Triangulo.java

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,10 +1,10 @@
 public class Triangulo extends FiguraBidimensional{
 
-    public Triangulo(int lado1, int lado2, int lado3){
-        super.lados = new int[] {lado1, lado2, lado3};
+    public Triangulo(int[] lados) throws ExcepcionMedidaMaxima {
+        super(lados);
     }
-    @Override
+
     public float calcularArea() {
-        return 0;
+        return 0.0F;
     }
 }
