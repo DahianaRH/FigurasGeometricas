544638506293f33e288822efc4b7a6c6939d74f3
DahianaRH <121201470+DahianaRH@users.noreply.github.com>
2023-04-14 22:22:39 -0500

    Update FiguraTridimensional.java
    Cambio de clase Abstracta a Interfaz

--- src/FiguraTridimensional.java
+++ src/FiguraTridimensional.java
@@ -1,16 +1,3 @@
-public abstract class FiguraTridimensional extends FiguraGeometrica{
-    public FiguraTridimensional(int[] lados) throws ExcepcionMedidaMaxima {
-        super(lados);
-    }
-
-    @Override
-    public int calcularPerimetro() { return 0; }
-
-    @Override
-    public String dibujar() { return null; }
-
-    @Override
-    public float calcularArea() { return 0; }
-
+public interface FiguraTridimensional {
     public abstract float calcularVolumen();
 }
