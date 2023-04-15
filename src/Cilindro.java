ad6cd2b348d1765e295a563bfe60cefe9c9a4ad9
DahianaRH <121201470+DahianaRH@users.noreply.github.com>
2023-04-14 22:18:59 -0500

    Update Cilindro.java

--- src/Cilindro.java
+++ src/Cilindro.java
@@ -1,4 +1,6 @@
-public class Cilindro extends FiguraTridimensional{
+import java.text.DecimalFormat;
+
+public class Cilindro extends FiguraGeometrica implements FiguraTridimensional{
 
     public Cilindro(int radio, int altura) throws ExcepcionMedidaMaxima {
         super(new int[] {radio, altura});
@@ -14,18 +16,22 @@ public class Cilindro extends FiguraTridimensional{
     @Override
     public float calcularArea() {
         float area = (float) (2 * Math.PI * getLados(0) * (getLados(0) + getLados(1)));
-        return area;
+        DecimalFormat df = new DecimalFormat("#.##");
+        String areaFormateada = df.format(area).replace(",", ".");
+        return Float.valueOf(areaFormateada);
     }
 
 
     @Override
     public float calcularVolumen() {
         float volumen = (float) (Math.PI * Math.pow(getLados(0), 2) * getLados(1));
-        return volumen;
+        DecimalFormat df = new DecimalFormat("#.##");
+        String volumenFormateado = df.format(volumen).replace(",", ".");
+        return Float.valueOf(volumenFormateado);
     }
 
     @Override
     public String dibujar() {
-        return ("El cilindro tiene un volumen de " + this.calcularVolumen() + ", con un perimetro de"+calcularPerimetro()+ "y un área de " + this.calcularArea());
+        return ("El cilindro tiene un volumen de " + this.calcularVolumen() + " y un área de " + this.calcularArea());
     }
 }
